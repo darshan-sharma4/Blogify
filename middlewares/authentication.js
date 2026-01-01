f6afdbd4aadb6e155560b747a2e757310ab03204
darshan-sharma4 <dksharma112004@gmail.com>
2026-01-01 22:35:28 +0530

    second

---
+++ middlewares/authentication.js
@@ -0,0 +1,22 @@
+
+const { verifyToken } = require("../services/authentication");
+
+function validateAuthenticationCookie(cookieName) {
+    return (req, res, next) => {
+        const tokenCookieValue = req.cookies[cookieName];
+        if (!tokenCookieValue) {
+            return next();
+        }
+        try {
+            const userPayload = verifyToken(tokenCookieValue)
+            req.user = userPayload;
+        } catch (error) {
+        }
+        return next();
+    }
+
+}
+
+module.exports = {
+    validateAuthenticationCookie,
+}
---
+++ services/authentication.js
@@ -0,0 +1,24 @@
+const JWT = require('jsonwebtoken')
+
+const secret = "$uperMan123@"
+
+function createTokenForUser(user) {
+    const payload = {
+        _id: user._id,
+        email: user.email,
+        profileImageUrl: user.profileImageUrl,
+        role: user.role,
+    };
+    const token = JWT.sign(payload, secret);
+    return token;
+};
+
+function verifyToken(token) {
+    const payload = JWT.verify(token, secret);
+    return payload;
+};
+
+module.exports = {
+    createTokenForUser,
+    verifyToken,
+}
